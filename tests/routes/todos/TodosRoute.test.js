2d14560a21bb2f967e248d416e37cad48a62b354
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-03-12 10:45:18 +0100

    corrected

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -11,3 +11,14 @@ describe("GET /v1/todos/all", () => {
     expect(response.body).toEqual([]);
   });
 });
+
+describe("GET /v1/todos/byid", () => {
+  test("responds with json", async () => {
+    const response = await request(app)
+      .get("/v1/todos/byid?todoId=1")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body.id).toEqual(1);
+  });
+});
