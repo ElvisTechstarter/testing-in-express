d95a91b711354118e726697800138c1a553adad5
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-03-12 12:27:30 +0100

    corrected errors

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -8,17 +8,25 @@ describe("GET /v1/todos/all", () => {
       .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body).toEqual([]);
+    const myTodos = response.body;
+    const myFirstTodo = myTodos[0];
+
+    expect(myTodos.length).toBeGreaterThan(0);
+    expect(myFirstTodo.id).toBeDefined();
+    expect(myFirstTodo.task).toBeDefined();
+    expect(myFirstTodo.userId).toBeDefined();
   });
 });
 
 describe("GET /v1/todos/byid", () => {
-  test("responds with json", async () => {
+  test("GET by Id", async () => {
+    const todoId = 1;
     const response = await request(app)
-      .get("/v1/todos/byid?todoId=1")
+      .get(`/v1/todos/byid?todoId=${todoId}`)
       .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body.id).toEqual(1);
+    const myTodo = response.body.todo;
+    expect(myTodo.id).toEqual(todoId);
   });
 });
--- tests/setup/jest.setup.js
+++ tests/setup/jest.setup.js
@@ -1,4 +1,5 @@
 require("../../src/server");
+const TodoModel = require("../../src/database/models/TodoModel");
 const todoSequelize = require("../../src/database/setup/database");
 
 module.exports = async () => {
@@ -8,9 +9,15 @@ module.exports = async () => {
     // });
     console.log("PRDDD", process.env);
     await todoSequelize.sync();
-    await todoSequelize.dropSchema("Todos");
+    await todoSequelize.dropSchema("todos");
     await todoSequelize.sync();
     // DB mit Daten füllen, um DB auf Test Szenarien vorzubereiten
+    await TodoModel.create({
+      userId: 1,
+      task: "Schlafen",
+      dueDate: "2024-03-12",
+      isDone: false,
+    });
     console.log("Test DB init");
   } catch (e) {
     console.error("MY DB Issue", e);
